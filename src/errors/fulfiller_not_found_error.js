71fa43391125d5510ba7696392a12f75a7b330ca
Victor Cuena Garcia <vcuenagarcia@cimpress.com>
2019-02-27 20:21:39 +0100

    Return error name

--- src/errors/fulfiller_not_found_error.js
+++ src/errors/fulfiller_not_found_error.js
@@ -1,5 +1,9 @@
 class FulfillerNotFoundError extends Error {
-
+    constructor(...args) {
+        super(...args)
+        this.name = 'FulfillerNotFoundError'
+    }
 }
+
 
 module.exports = FulfillerNotFoundError;
